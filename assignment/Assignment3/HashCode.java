3dad4c4f2dc7b6f5778bf47c1be75be022ba8810
nehagarg <nehaagarwal_14@yahoo.co.in>
2016-01-13 11:09:35 -0800

    updated

--- assignment/Assignment3/HashCode.java
+++ assignment/Assignment3/HashCode.java
@@ -3,7 +3,17 @@ package Assignment3;
 public class HashCode {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	Strins s = "hello";
+	System.out.println("before" + s.hashCode());
+	
+	s = s + "world";
+	System.out.println("after" + s.hashCode());
+	
+	StringBuffer buff = new Stringbuffer(s);
+	System.out.println("before" + buff.hashCode());
+	
+	buff= buff.append("world");
+	System.out.println("after" + buff.hashCode());
 
 	}
 
