b9a4e8511ad9da34e4000189369eb6f6eb394042
nehagarg <nehaagarwal_14@yahoo.co.in>
2016-01-31 15:06:28 -0800

    updated

--- Assignment8/Assignment8/GarbageCollection.java
+++ Assignment8/Assignment8/GarbageCollection.java
@@ -3,8 +3,12 @@ package Assignment8;
 public class GarbageCollection {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		StringBuffer greet = new StringBuffer("hello");
+		System.out.println(greet);
+		greet = null;
+		System.gc();
+		System.out.println(greet);
 	}
 
 }
