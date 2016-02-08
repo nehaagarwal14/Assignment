b186ab0bb8f71afc18d8516c34a793e83eda628b
nehagarg <nehaagarwal_14@yahoo.co.in>
2016-02-07 18:08:45 -0800

    updated

--- TestngAssignment/src/com/test/StringSortTest.java
+++ TestngAssignment/src/com/test/StringSortTest.java
@@ -9,10 +9,25 @@ import static org.testng.AssertJUnit.assertEquals;
 import com.main.StringSort;
 
 public class StringSortTest {
+	@Test
+	public void method(){
+		String str = "neha";
+		StringSort sr = new StringSort();
+		String actual= sr.Sort(str);
+		assertEquals("aehn",actual);
+	}
+	
+	@Test(expectedExceptions = NullPointerException.class)
+	public void method1(){
+		String str = null;
+		StringSort sr = new StringSort();
+		String actual = sr.Sort(str);
+		assertEquals("null",actual);	
+	}	
 	
 	@Test(testName = "stringInput")
 	@Parameters({"stringInput"})
-	public void method1(@Optional("bharti")String name){
+	public void method2(@Optional("bharti")String name){
 		StringSort sr = new StringSort();
 		String actual = sr.Sort(name);
 		assertEquals("abhirt", actual);
@@ -26,7 +41,7 @@ public class StringSortTest {
 	}
 	
 	@Test(dataProvider = "data")
-	public void method2(String input,String expected){
+	public void method3(String input,String expected){
 		StringSort sr = new StringSort();
 		String actual = sr.Sort(input);
 		assertEquals(expected, actual);
